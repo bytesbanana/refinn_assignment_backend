d1ae8cf17f78f654c4768ec6eebfb41e981b336a
Saharat Paynok <saharat.paynok@gmail.com>
2022-09-20 00:40:17 +0700

    optimize import

--- src/main/java/com/refinninterview/demo/repository/AssetRepository.java
+++ src/main/java/com/refinninterview/demo/repository/AssetRepository.java
@@ -1,7 +1,6 @@
 package com.refinninterview.demo.repository;
 
 import com.refinninterview.demo.domain.Asset;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/refinninterview/demo/web/controller/AssetController.java
+++ src/main/java/com/refinninterview/demo/web/controller/AssetController.java
@@ -1,32 +1,26 @@
 package com.refinninterview.demo.web.controller;
 
+import com.refinninterview.demo.dto.AssetDTO;
 import com.refinninterview.demo.dto.PaginateAssetDTO;
 import com.refinninterview.demo.service.AssetService;
-import com.refinninterview.demo.dto.AssetDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
